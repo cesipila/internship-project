199d92ea1c001c6ab34529d656bf34c2b8779906
Charles Esipila <cesipila@gmail.com>
2024-05-30 15:57:42 -0400

    Cross-Browser and Headless browser changes

--- internship-project/features/environment.py
+++ internship-project/features/environment.py
@@ -5,61 +5,60 @@ from selenium.webdriver.support.wait import WebDriverWait
 from webdriver_manager.chrome import ChromeDriverManager
 from webdriver_manager.firefox import GeckoDriverManager
 
-
 from app.application import Application
 from support.logger import logger
+
 
 # Run Behave tests with Allure results
 # behave -f allure_behave.formatter:AllureFormatter -o test_results/ features/tests/target_app_ui_tests.feature
 
 
 def browser_init(context):
-#     """
-#     :param context: Behave context
-#     """
+    #     """
+    #     :param context: Behave context
+    #     """
 
-### IN THE EVENT OF CHROME ONLY TESTS ###
-    driver_path = ChromeDriverManager().install()
-    service = Service(driver_path)
-    context.driver = webdriver.Chrome(service=service)
-
-### IN THE EVENT OF FIREFOX ONLY TESTS ###
-    # driver_path = GeckoDriverManager().install()
+    ### IN THE EVENT OF CHROME ONLY TESTS ###
+    # driver_path = ChromeDriverManager().install()
     # service = Service(driver_path)
+    # context.driver = webdriver.Chrome(service=service)
+
+    ### IN THE EVENT OF FIREFOX ONLY TESTS ###
+    driver_path = GeckoDriverManager().install()
+    service = Service(driver_path)
+    context.driver = webdriver.Firefox(service=service)
+
+    ### BROWSERS WITH DRIVERS: provide pathto the driver file ###
+    # service = Service(executable_path='/Users/cesip/python-selenium-automation/geckodriver.exe'
     # context.driver = webdriver.Firefox(service=service)
 
-### BROWSERS WITH DRIVERS: provide pathto the driver file ###
-# service = Service(executable_path='/Users/cesip/python-selenium-automation/geckodriver.exe'
-# context.driver = webdriver.Firefox(service=service)
+    ### IN THE EVENT OF SAFARI ONLY TESTS: MAC OS ONLY ###
+    # context.driver = webdriver.Safari()
 
-### IN THE EVENT OF SAFARI ONLY TESTS: MAC OS ONLY ###
-# context.driver = webdriver.Safari()
+    ### HEADLESS MODE ###
+    # options = webdriver.ChromeOptions()
+    # options.add_argument('headless')
+    # service = Service(ChromeDriverManager().install())
+    # context.driver = webdriver.Chrome(
+    #     options=options,
+    #     service=service
+    # )
 
-### HEADLESS MODE ###
-# options = webdriver.ChromeOptions()
-# options.add_argument('headless')
-# service = Service(ChromeDriverManager().install())
-# context.driver = webdriver.Chrome(
-#     options=options,
-#     service=service
-# )
-
-### BROWSERSTACK ###
-# Register for Browserstack, then grab it from https://www.browserstack.com/accounts/settings
-# bs_user = 'add username'
-# bs_key = 'add_key_fromsite'
-# url = f'http://{bs_user}:{bs_key}@hub.browserstack.com/wd/hub'
-#
-# options = Options()
-# bstack_options = {
-#     'os' : 'Windows',
-#     'osVersion' : '10',
-#     'browserName' : 'Firefox',
-#     'sessionName' : scenario_name
-# }
-# options.set_capability('bstack:options', bstack_options)
-# context.driver = webdriver.Remote(command_executor=url, options=options)
-
+    ### BROWSERSTACK ###
+    # Register for Browserstack, then grab it from https://www.browserstack.com/accounts/settings
+    # bs_user = 'add username'
+    # bs_key = 'add_key_fromsite'
+    # url = f'http://{bs_user}:{bs_key}@hub.browserstack.com/wd/hub'
+    #
+    # options = Options()
+    # bstack_options = {
+    #     'os' : 'Windows',
+    #     'osVersion' : '10',
+    #     'browserName' : 'Firefox',
+    #     'sessionName' : scenario_name
+    # }
+    # options.set_capability('bstack:options', bstack_options)
+    # context.driver = webdriver.Remote(command_executor=url, options=options)
 
     context.driver.maximize_window()
 
@@ -71,7 +70,7 @@ def browser_init(context):
 
 
 def before_scenario(context, scenario):
-    #print('\nStarted scenario: ', scenario.name)
+    # print('\nStarted scenario: ', scenario.name)
     logger.info(f'Started scenario: {scenario.name}')
     browser_init(context)
 
--- internship-project/pages/secondary.py
+++ internship-project/pages/secondary.py
@@ -1,4 +1,4 @@
-from telnetlib import EC
+from selenium.webdriver.support import expected_conditions as EC
 
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
@@ -13,34 +13,42 @@ class Secondary(Page):
     FILTER_BTN = (By.CSS_SELECTOR, "div[wized*='openFiltersWindow']")
     WANT_TO_SELL = (By.CSS_SELECTOR, "div[wized*='ListingTypeSell']")
     APPLY_FILTER_BTN = (By.CSS_SELECTOR, "a[wized*='applyFilter']")
+    WAIT_PAGE_LOAD = (By.CSS_SELECTOR, "img[class='unit-plan-image']")
 
-    def open_secondary_page(self):
+    def open_secondary_page(self, timeout=10):
         self.open('https://soft.reelly.io/secondary-listings')
-        # calls base page method directly, passes url
 
     def verify_secondary_url(self):
         # self.verify_partial_url('/secondary=listings')
         self.verify_url('https://soft.reelly.io/secondary-listings')
 
     def verify_for_sale_tags(self):
-        elements = self.driver.find_elements(By.XPATH, "//div[contains(text(), 'Want to buy')]")
+        elements = self.driver.find_elements(*self.FOR_SALE_TAG)
         assert len(elements) < 2, (f"Expected fewer than 2 elements with the text 'Want to buy', "
                                    f"but found {len(elements)}")
 
     def apply_filters(self, timeout=10):
+        # Initial sleep to wait for Firefox page to start loading
+        # sleep(3)
+
         # Set implicit wait
         self.driver.implicitly_wait(timeout)
 
+        # Initial wait for Firefox page to finish loading
+        # WebDriverWait(self.driver, timeout).until(EC.visibility_of_element_located(self.WAIT_PAGE_LOAD))
+
+
         # Click the filters button
-        filters_btn = self.driver.find_element(*self.FILTER_BTN)
+        filters_btn = WebDriverWait(self.driver, timeout).until(EC.element_to_be_clickable(self.FILTER_BTN))
         filters_btn.click()
 
         # Click the want to sell listing type
-        listing_filter = self.driver.find_element(*self.WANT_TO_SELL)
+        listing_filter = WebDriverWait(self.driver,timeout).until(EC.element_to_be_clickable(self.WANT_TO_SELL))
         listing_filter.click()
 
         # Click the apply filter button
-        apply_filter_btn = self.driver.find_element(*self.APPLY_FILTER_BTN)
+        apply_filter_btn = WebDriverWait(self.driver,timeout).until(EC.element_to_be_clickable(self.APPLY_FILTER_BTN))
         apply_filter_btn.click()
 
-        self.driver.implicitly_wait(0)  # Reset implicit wait to default
+        # Reset implicit wait to default
+        self.driver.implicitly_wait(0)
